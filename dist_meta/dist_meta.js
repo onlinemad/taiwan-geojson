ae6b0f4fe09c1d94b95277bc06e1bedbfd70e15c
Ian Wu <onlinemad@gmail.com>
2016-10-24 16:19:31 +0800

    Fix 宜蘭縣英文無法 mapping 的問題

--- dist_meta/dist_meta.js
+++ dist_meta/dist_meta.js
@@ -12,7 +12,8 @@ for(var dist in zipcode_dist_key) {
       zipcode: zip,
       en: zipcode_zh_en[zip].name_en.split(',')[0]
     }
-    dist_meta[dist].en = zipcode_zh_en[zip].name_en.split(',')[1]
+    var dist_en = zipcode_zh_en[zip].name_en.split(',')[1]
+    if (dist_en) dist_meta[dist].en = dist_en
   }
 }
 fs.writeFile('dist_meta.json', JSON.stringify(dist_meta, null, 2), (err) => {
